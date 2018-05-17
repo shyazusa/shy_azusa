9311f56890f60e40c0ddf0e8d8972cfd7700f1e1
shy_azusa <shy.azusa@gmail.com>
2018-05-17 15:13:22 +0900

    Use space instead of tab

--- source/cli.js
+++ source/cli.js
@@ -10,13 +10,13 @@ const {h, render} = require('ink');
 const ui = importJsx('./ui');
 
 meow(`
-	Usage
-	  $ sindresorhus
+  Usage
+    $ sindresorhus
 `);
 
 const fallback = async () => {
-	const image = await terminalImage.file(path.join(__dirname, 'avatar-fallback.png'));
-	console.log(image);
+  const image = await terminalImage.file(path.join(__dirname, 'avatar-fallback.png'));
+  console.log(image);
 };
 
 termImg(path.join(__dirname, 'avatar.png'), {fallback});
